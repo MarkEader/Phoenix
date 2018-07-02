cf0b923534fd188d2a25a6613e5a6ce4519688ea
Mark Eader <mark.eader@brockgroup.com>
2018-07-02 09:03:54 -0500

    fix broken build

--- Phoenix.API/Controllers/TimeSheetController.cs
+++ Phoenix.API/Controllers/TimeSheetController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Phoenix.Models;
 
 namespace Phoenix.API.Controllers
 {
     [Route("api/[controller]")]
     public class TimeSheetController : Controller
     {
-        public TimeSheetController()
+        private readonly PhoenixDbContext _context;
+        private readonly IMapper _mapper;
+
+        public TimeSheetController(PhoenixDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
     }
 }
